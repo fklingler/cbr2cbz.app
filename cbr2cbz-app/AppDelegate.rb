83a207210708af19d07c9347d68f6edc6b86cc3b
François KLINGLER <francois@fklingler.com>
2012-11-23 20:52:20 -0500

    First working version

--- cbr2cbz-app/AppDelegate.rb
+++ cbr2cbz-app/AppDelegate.rb
@@ -1,15 +1,70 @@
-#
-#  AppDelegate.rb
-#  cbr2cbz-app
-#
-#  Created by François KLINGLER on 23/11/12.
-#  Copyright 2012 François KLINGLER. All rights reserved.
-#
+require 'rubygems'
+require 'cbr2cbz/converter'
 
 class AppDelegate
-    attr_accessor :window
-    def applicationDidFinishLaunching(a_notification)
-        # Insert code here to initialize your application
+  attr_accessor :window
+  
+  def applicationDidFinishLaunching(a_notification)
+    @window.releasedWhenClosed = false
+    
+    converter = Cbr2cbz::Converter.new
+    @cbr2cbz_converter = converter
+    @window.instance_eval { @cbr2cbz_converter = converter }
+    
+    @window.extend DragMethods
+
+    @window.registerForDraggedTypes NSArray.arrayWithObject(NSFilenamesPboardType)
+  end
+  
+  module DragMethods
+    def draggingEntered(sender)
+      NSDragOperationEvery
     end
+    
+    def performDragOperation(sender)
+      pboard = sender.draggingPasteboard
+      files = pboard.propertyListForType NSFilenamesPboardType
+      
+      @cbr2cbz_converter.convert files
+    end
+  end
+
+  def menu_convert(sender)
+    panel = NSOpenPanel.openPanel
+    panel.allowsMultipleSelection = true
+    panel.canChooseDirectories = true
+    
+    panel.beginWithCompletionHandler ->(result) do
+      if result == NSFileHandlingPanelOKButton
+        @cbr2cbz_converter.convert panel.URLs.map(&:path)
+      end
+    end
+  end
+
+  def application(sender, openFiles:files)
+    @cbr2cbz_converter.convert files
+  end
+
+  def applicationShouldHandleReopen(application, hasVisibleWindows: hasVisibleWindows)
+    @window.makeKeyAndOrderFront(nil)
+  end
 end
 
+# Monkey patch to search for unrar in different paths 
+module Cbr2cbz
+  class Converter
+    PATH = ["/usr/local/bin", "/usr/bin", "/bin", "/opt/local/bin"]
+    
+    alias :old_initialize :initialize
+    def initialize(options = {})
+      old_initialize(options)
+      @unrar = PATH.find do |path|
+        File.exists? "#{path}/unrar"
+      end << "/unrar"
+    end
+    
+    def unrar(filename, folder)
+      `#{@unrar} e "#{filename}" "#{folder}/"`
+    end
+  end
+end
